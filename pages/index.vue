f022a9ec184586406f490e089db17df061dbac6f
Paul Halliday <hello@paulhalliday.io>
2021-10-22 04:25:10 +0100

    Added counter

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,34 @@
 <template>
-  <h1 class="text-4xl text-red-500 bg-gray-300"></h1>
+  <div class="flex flex-col items-center justify-center h-full space-y-4 text-xl">
+    <h2>
+      This is my first Nuxt 3 app
+    </h2>
+    <p class="">Here's a counter:</p>
+    <div class="flex items-center justify-center">
+      
+      <button @click="counter++" class="px-4 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-700">
+        +
+      </button>
+      <span class="mx-2">{{ counter }}</span>
+      <button @click="counter--" class="px-4 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-700">
+        -
+      </button>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  layout: 'custom',
+  setup() {
+    const counter = ref(0)
+
+    return {
+      counter
+    }
+  }
+})
+</script>
 
